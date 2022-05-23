a7517ca6e4b65db1b7c8aad9b0f4007bd2db0ec2
yannsecundo <yannsecundo34@gmail.com>
2022-05-23 17:55:40 -0400

    depois de muita luta e uma olhadinha no course, requisito 1 cumprido

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,10 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  if (ids === '') {
+    return [];
+  }
+  const { species } = data;
+  return species.filter(({ id }) => ids.includes(id));
 }
-
 module.exports = getSpeciesByIds;
