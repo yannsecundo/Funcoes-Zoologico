dbc0e96a0b13bfd18b0918007eb46973250a8ad2
yannsecundo <yannsecundo34@gmail.com>
2022-05-24 15:59:12 -0400

    requisito 2, com teste do requisito comentado

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,10 @@
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  return data.species.find((animalNomeTest) => animalNomeTest.name === animal)
+    .residents.every(({ age: idadeAnimal }) => idadeAnimal > age);
 }
+// teste da função
+// console.log(getAnimalsOlderThan('otters', 7));
 
 module.exports = getAnimalsOlderThan;
