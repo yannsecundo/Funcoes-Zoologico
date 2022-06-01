53a54f130fb9287d199352d5d38f6fde36dc570b
yannsecundo <yannsecundo34@gmail.com>
2022-06-01 10:18:18 -0400

    requisitos 9 e 10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,15 @@
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const primeiraEspecie = data.employees.find(({ id: idEspecie }) => idEspecie === id)
+    .responsibleFor[0];
+  const especieMaisVelha = data.species.find(({ id: idEspecie }) => idEspecie === primeiraEspecie)
+    .residents
+    .reduce((old, resident) => {
+      if (old.age > resident.age) return old;
+      return resident;
+    });
+  return Object.values(especieMaisVelha);
 }
 
 module.exports = getOldestFromFirstSpecies;
--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,39 @@
 const data = require('../data/zoo_data');
 
+const diaFechado = () => ({
+  officeHour: 'CLOSED',
+  exhibition: 'The zoo will be closed!',
+});
+
+const animaisEmExibicao = (day) => data.species.filter(({ availability }) => availability
+  .includes(day))
+  .map(({ name }) => name);
+
+const determinandoDiaFechado = (day) => {
+  if (day === 'Monday') return diaFechado();
+
+  const { open, close } = data.hours[day];
+
+  return {
+    officeHour: `Open from ${open}am until ${close}pm`,
+    exhibition: animaisEmExibicao(day),
+  };
+};
+
 function getSchedule(scheduleTarget) {
-  // seu código aqui
+  const arrayAnimais = data.species.map(({ name }) => name);
+  const arrayDias = Object.keys(data.hours);
+
+  if (scheduleTarget === 'Monday') {
+    return { Monday: diaFechado() };
+  }
+  if (arrayDias.includes(scheduleTarget)) {
+    return { [scheduleTarget]: determinandoDiaFechado(scheduleTarget) };
+  }
+  if (arrayAnimais.includes(scheduleTarget)) {
+    return data.species.find(({ name }) => name === scheduleTarget).availability;
+  }
+  return arrayDias.reduce((objeto, day) => ({ ...objeto, [day]: determinandoDiaFechado(day) }), {});
 }
 
 module.exports = getSchedule;
