fe51d5530b7bcba903bbbbb21b93c025bc18ecfe
yannsecundo <yannsecundo34@gmail.com>
2022-06-01 17:57:51 -0400

    requisito 14

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,31 @@
 const data = require('../data/zoo_data');
 
-function getAnimalMap(options) {
-  // seu código aqui
+const arrayNomes = (sex, sorted, residents) => {
+  if (sorted && sex) {
+    return residents.filter(({ sex: filter }) => filter === sex).map(({ name }) => name).sort();
+  }
+  if (sorted) {
+    return residents.map(({ name }) => name).sort();
+  }
+  if (sex) {
+    return residents.filter(({ sex: filter }) => filter === sex).map(({ name }) => name);
+  }
+  return residents.map(({ name }) => name);
+};
+
+const objNomes = (sex, sorted) => data.species
+  .reduce((objeto, { name, location, residents }) => ({ ...objeto,
+    [location]: [...(objeto[location] || []),
+      { [name]: arrayNomes(sex, sorted, residents) }] }), {});
+
+const objetoDefault = () => data.species
+  .reduce((obj, { name, location }) =>
+    ({ ...obj, [location]: [...(obj[location] || []), name] }), {});
+
+function getAnimalMap(options = {}) {
+  const { includeNames, sorted, sex } = options;
+  if (!includeNames) return objetoDefault();
+  if (includeNames) return objNomes(sex, sorted);
 }
 
 module.exports = getAnimalMap;
